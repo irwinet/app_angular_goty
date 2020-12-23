02108fa4f0ec3bf8b97022af64fe396338ecb814
irwinet <irwinestradatorres@gmail.com>
2020-12-23 15:09:28 -0500

    Add AppRoutingModule And Structure Project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { GotyComponent } from './pages/goty/goty.component';
+
+const routes: Routes = [
+    { path: 'inicio', component: InicioComponent },
+    { path: 'goty', component: GotyComponent },
+    { path: '**', pathMatch: 'full', component: InicioComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
